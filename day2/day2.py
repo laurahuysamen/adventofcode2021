0b10d62129debc37f146c9fe41f9c09b1ee95cad
Laura Huysamen <laura.huysamen@outsystems.com>
2021-12-02 05:16:43 +0000

    day2, part 1 & 2

---
+++ day2/day2.py
@@ -0,0 +1,66 @@
+
+def read_file():
+    with open('input.txt') as f:
+        lines = f.readlines()
+    return lines
+
+def main():
+    part1()
+    part2()
+
+def part1(): 
+    lines = read_file()
+    horizontalposition = 0
+    depth = 0
+    
+    for command in lines: 
+        instructions = command.split()
+        direction = instructions[0]
+        unit = int(instructions[1])
+        
+        if direction == "forward":
+            horizontalposition = horizontalposition + unit
+            
+        elif direction == "down":
+            depth = depth + unit
+            
+        elif direction == "up":
+            depth = depth - unit
+        else:
+            print("OH NO")
+            
+    print("PART 1:")ok
+    print("Horizontal: " + str(horizontalposition))
+    print("Depth: " + str(depth))
+    print("Multiplied: " + str(depth*horizontalposition))
+
+def part2():
+    lines = read_file()
+    horizontalposition = 0
+    depth = 0
+    aim = 0 
+    
+    for command in lines: 
+        instructions = command.split()
+        direction = instructions[0]
+        unit = int(instructions[1])
+        
+        if direction == "forward":
+            horizontalposition = horizontalposition + unit
+            depth = depth + (aim * unit)
+            
+        elif direction == "down":
+            aim = aim + unit
+            
+        elif direction == "up":
+            aim = aim - unit
+        else:
+            print("OH NO")
+            
+    print("PART 2:")
+    print("Horizontal: " + str(horizontalposition))
+    print("Depth: " + str(depth))
+    print("Multiplied: " + str(depth*horizontalposition))
+    
+if __name__ == "__main__":
+    main()
